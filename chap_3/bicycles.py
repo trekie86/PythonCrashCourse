32ab9204476e403fb8bb9a02bec9865a9702edbc
Rob <tekie86@gmail.com>
2019-09-01 10:44:33 -0700

    Chapter 3 complete
    I learned about list manipulation

---
+++ chap_3/bicycles.py
@@ -0,0 +1,5 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+message = f"My first bicycle was {bicycles[0].title()}"
+print(message)
---
+++ chap_3/cars.py
@@ -0,0 +1,8 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+
+print(cars)
+print(sorted(cars, reverse=True))
+
+cars.sort(reverse=True)
+print(cars)
