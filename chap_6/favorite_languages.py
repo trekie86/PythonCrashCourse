6b8904b4562dd78643fe0b9b362375436968e7bd
Rob <trekie86@gmail.com>
2020-08-19 22:05:38 -0700

    Through the aliens section

---
+++ chap_6/favorite_languages.py
@@ -0,0 +1,17 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+#for name, language in favorite_languages.items():
+#    print(f"{name.title()}'s favorite language is {language.title()}")
